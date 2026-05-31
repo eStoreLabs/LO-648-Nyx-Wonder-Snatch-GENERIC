e1df420440fc4126192ae58a29278888de1fa2e6
Roman Pavlenko <rpavlenko.mail@gmail.com>
2026-05-31 22:22:38 +0200

    feat: update markup

--- scripts/initalizeSwiper.js
+++ scripts/initalizeSwiper.js
@@ -1,6 +1,6 @@
 (() => {
   const swiper = new Swiper('#es-rc #es-rc-content .es-swiper', {
-    enabled: false, // start disabled for mobile
+    enabled: true, // start disabled for mobile
     slidesPerView: 1,
     loop: true,
     autoHeight: true,
@@ -12,10 +12,10 @@
       nextEl: '#es-button-next',
       prevEl: '#es-button-prev',
     },
-    breakpoints: {
-      756: {
-        enabled: true,
-      },
-    },
+    // breakpoints: {
+    //   756: {
+    //     enabled: true,
+    //   },
+    // },
   });
 })();
